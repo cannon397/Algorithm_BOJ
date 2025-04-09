15910f289e813b2420bb30eeebf121743509c8d7
SeungJi <63600525+cannon397@users.noreply.github.com>
2025-04-09 21:18:18 +0900

    [level 1] Title: 대충 만든 자판, Time: 52.38 ms, Memory: 94.1 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
@@ -5,7 +5,6 @@ class Solution {
         for(int i = 0; i < targets.length; i++){
             for(int j = 0; j < targets[i].length(); j++){
                 int temp = Integer.MAX_VALUE;
-                boolean flag = false;
                 for(int k = 0; k < keymap.length; k++){
                     for(int l = 0; l < keymap[k].length(); l++){
                         if(keymap[k].charAt(l) == targets[i].charAt(j)){
