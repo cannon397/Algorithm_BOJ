2b5171ac06fb86e6ccbf972b7fc6eb5a48c26e29
SeungJi <63600525+cannon397@users.noreply.github.com>
2025-04-09 21:15:26 +0900

    [level 1] Title: 대충 만든 자판, Time: 39.70 ms, Memory: 87.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class Solution {
+    public int[] solution(String[] keymap, String[] targets) {
+        int[] answer = new int[targets.length];
+        for(int i = 0; i < targets.length; i++){
+            for(int j = 0; j < targets[i].length(); j++){
+                int temp = Integer.MAX_VALUE;
+                boolean flag = false;
+                for(int k = 0; k < keymap.length; k++){
+                    int l;
+                    for(l = 0; l < keymap[k].length(); l++){
+                        if(keymap[k].charAt(l) == targets[i].charAt(j)){
+                            temp = Math.min(l + 1, temp);
+                            break;
+                        }
+                    }
+                }
+                if(temp == Integer.MAX_VALUE){
+                    answer[i] = -1;
+                } else if(answer[i] != -1){
+                    answer[i] += temp;
+                }
+            }
+        }
+        for(int i = 0; i < answer.length; i++){
+            if(answer[i] == 0){
+                answer[i] = -1;
+            }
+        }
+        return answer;
+    }
+}
