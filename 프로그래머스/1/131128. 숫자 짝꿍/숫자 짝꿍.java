e604bd72585fcc0879221ecfabe6ba11aea6c066
SeungJi <63600525+cannon397@users.noreply.github.com>
2025-04-11 22:16:04 +0900

    [level 1] Title: 숫자 짝꿍, Time: 88.26 ms, Memory: 155 MB -BaekjoonHub

---
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class Solution {
+    public String solution(String X, String Y) {
+        String answer = "";
+        int[] x = new int[10];
+        int[] y = new int[10];
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < X.length(); i++){
+            x[X.charAt(i) - '0']++;
+        }
+        for(int i = 0; i < Y.length(); i++){
+            y[Y.charAt(i) - '0']++;
+        }
+        for(int i = 9; i >= 0; i--){
+            int min = Math.min(x[i],y[i]);
+            for(int j = 0; j < min; j++){
+               sb.append(i); 
+            }
+        }
+        
+        if(sb.length() == 0){
+            answer = "-1";
+        }else if(sb.charAt(0) == '0'){
+            answer = "0";
+        }else{
+            answer = sb.toString(); 
+        }
+        
+        return answer;
+    }
+}
