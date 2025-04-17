a11933fc05e5a76c24f3c35b7c323c7660648299
SeungJi <63600525+cannon397@users.noreply.github.com>
2025-04-17 21:27:14 +0900

    [level 2] Title: 튜플, Time: 75.93 ms, Memory: 100 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -3,19 +3,15 @@ class Solution {
     public int[] solution(String s) {
         int[] answer = {};
         HashMap<Integer, Integer> map = new HashMap<>();
-        s = s.replace("{","");
-        s = s.replace("}","");
-        String[] numbers = s.split(",");
-        StringBuilder sb = new StringBuilder();
+        String[] numbers = s.replace("{","").replace("}","").split(",");
         for(String number : numbers){
             int n = Integer.parseInt(number);
             map.put(n, map.getOrDefault(n, 0) + 1);
         }
         answer = new int[map.size()];
         ArrayList<Map.Entry<Integer,Integer>> list = new ArrayList<>(map.entrySet());
-        list.sort((o1, o2) -> o2.getValue() - o1.getValue());
         for(int i = 0; i < answer.length; i++){
-            answer[i] = list.get(i).getKey();
+            answer[answer.length - list.get(i).getValue()] = list.get(i).getKey();
         }
         return answer;
     }
