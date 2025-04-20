051b1570227cdbbf5a9da34504a52147c927419e
SeungJi <63600525+cannon397@users.noreply.github.com>
2025-04-20 21:53:38 +0900

    [level 2] Title: 택배상자, Time: 58.76 ms, Memory: 157 MB -BaekjoonHub

---
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] order) {
+        int answer = 0;
+        Stack<Integer> subBelt = new Stack<>();
+        int mainBelt = 1;
+        int index = 0;
+        while(mainBelt <= order.length){
+            if(order[index] != mainBelt){
+                subBelt.push(mainBelt);
+                mainBelt++;  
+            }else{
+                index++;
+                answer++;
+                mainBelt++;
+            }
+            if(order[index] != mainBelt){
+                while(!subBelt.isEmpty() && subBelt.peek() == order[index]){
+                    subBelt.pop();
+                    answer++;
+                    index++;
+                }
+            }
+        }
+        //System.out.println(subBelt);
+        return answer;
+    }
+}
