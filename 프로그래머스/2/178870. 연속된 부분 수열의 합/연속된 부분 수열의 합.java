331f0d0e818af0050009f1a3b3ae7d352aa5a0bc
SeungJi <63600525+cannon397@users.noreply.github.com>
2025-04-27 23:46:53 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 11.59 ms, Memory: 155 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
@@ -2,30 +2,20 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] sequence, int k) {
         int[] answer = new int[2];
+        int left = 0, right = 0;
+        int sum = 0;
         int count = Integer.MAX_VALUE;
-        int[] dp = new int[sequence.length + 1];
-        dp[0] = 0;
-        for(int i = 1; i < dp.length;i++){
-            dp[i] = dp[i - 1] + sequence[i - 1];
-        }
-        int j = 0;
-        //stem.out.println(Arrays.toString(dp));
-        for(int i = 1; i < dp.length; i++){
-            //if(i < 0 || j < 0 || i >= dp.length || j >= dp.length) continue;
-            if(i == j && sequence[i - 1] == k){
-                answer[0] = i - 1;
-                answer[1] = i - 1;
-                break;
+        while(right < sequence.length){
+            sum += sequence[right++];
+            while(sum > k && left < right){
+                sum -= sequence[left++];
             }
-            if(dp[i] - dp[j] == k && count > i - j){
-                count = Math.min(count, i - j);
-                //stem.out.printf("j:%d, i:%d\n",j, i - 1);
-                answer[0] = j;
-                answer[1] = i - 1;
-            }else if(dp[i] - dp[j] > k){
-                i--;
-                j++;
+            if(sum == k && count > right - left) {
+                count = Math.min(count, right - left);
+                answer[0] = left; 
+                answer[1] = right - 1;
             }
+
         }
         return answer;
     }
