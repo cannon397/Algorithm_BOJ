b45f0cd2d7e1dfe12502f2862d1bb97bd609d027
SeungJi <63600525+cannon397@users.noreply.github.com>
2025-05-10 22:15:14 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 25.28 ms, Memory: 131 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/README.md
+++ 프로그래머스/2/135807. 숫자 카드 나누기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 숫자 카드 나누기 - 135807 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/135807) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/135807#) 
 
 ### 성능 요약
 
-메모리: 153 MB, 시간: 25.00 ms
+메모리: 131 MB, 시간: 25.28 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 10일 22:08:12
+2025년 05월 10일 22:15:11
 
 ### 문제 설명
 
--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
@@ -4,37 +4,21 @@ class Solution {
         int n = arrayA.length;
         int a = arrayA[0];
         int b = arrayB[0];
-        if(n == 1) return (a % b == 0 || b % a == 0) ? 0 : (a > b) ? a : b;
         for(int i = 1; i < n; i++){
             a = gcd(a, arrayA[i]);
             b = gcd(b, arrayB[i]);
         }
-        //System.out.printf("a: %d, b: %d", a, b);
-        boolean flag1 = true, flag2 = true;
-        for(int i = 0; i < n; i++){
-            if(arrayB[i] % a == 0) {
-                flag1 = false;
-                break;
+        int candidateA = isValid(a, arrayB) ? a : 0;
+        int candidateB = isValid(b, arrayA) ? b : 0;
+        return Math.max(candidateA, candidateB);
+    }
+    public boolean isValid(int gcd, int[] array){
+        for(int i = 0; i < array.length; i++){
+            if(array[i] % gcd == 0) {
+                return false;
             }
         }
-        for(int i = 0; i < n; i++){
-            if(arrayA[i] % b == 0) {
-                flag2 = false;
-                break;
-            }
-        }
-        if(flag1 && !flag2){
-            return (a > 1) ? a : 0;
-        }else if(!flag1 && flag2){
-            return (b > 1) ? b : 0;
-        }else if(flag1 && flag2){
-            return (a > b) ? a : b;
-        }else if(!flag1 && !flag2){
-            return 0;
-        }
-        
-        //answer = (a > b) ? a : b;
-        return answer;
+        return true;
     }
     public int gcd(int a, int b){
         while(b != 0){
